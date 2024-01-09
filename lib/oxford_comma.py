21084e6905815b7dfbe24b79f2812094474acf2d
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-09 08:52:52 -0500

    redo

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,26 +1,33 @@
-import ipdb
-def oxford_comma(items):
-      if len(items) == 1:
-         return items[0]
-      elif len(items) == 2:
-         return ' and '.join(items)
-      else:
-        items[-1] = 'and ' + items[-1]
-        return ', '.join(items)
+# import ipdb
+# def oxford_comma(items):
+#       if len(items) == 1:
+#          return items[0]
+#       elif len(items) == 2:
+#          return ' and '.join(items)
+#       else:
+#         items[-1] = 'and ' + items[-1]
+#         return ', '.join(items)
 
+# print(oxford_comma(['kiwi', 'banana', 'hel']))
+
+
+
+def oxford_comma(list):
+    if len(list) == 1:
+        return list[0]
+    elif len(list) == 2:
+        return ' and '.join(list)
+    elif len(list) >= 3:
+      list[-1] = 'and ' + list[-1]
+      print(list)
+      return ', '.join(list)
 print(oxford_comma(['kiwi', 'banana', 'hel']))
+    
+   
 
-
-
-
-
-# Write a function oxford_comma() in the lib/oxford_comma.py file that takes an array of string elements as 
-# an argument and converts it into a string using the Oxford commaLinks to an external site..
+# Write a function oxford_comma() in the lib/oxford_comma.py file 
+# that takes an array of string elements as an argument and converts
+# it into a string using the Oxford commaLinks to an external site..
 
 # oxford_comma(["fiddleheads", "okra", "kohlrabi"])
 # # => "fiddleheads, okra, and kohlrabi"
-# Hint: You will need to refer to the section above about converting lists into strings, but note that coding
-# this function will involve a couple of extra challenges.
-
-# This might be a challenging lab, so take your time using Google and playing around with your code.
-#  Good luck and have fun!
